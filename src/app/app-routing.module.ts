e5446f30393a6845505c65928f05aaefd2bd346a
DevinDon <DevinDon@Foxmail.com>
2018-11-01 22:53:22 +0800

    Update: 更新路由模块.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { InboxComponent } from './component/inbox/inbox.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'inbox', component: InboxComponent },
+  { path: '**', component: InboxComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
