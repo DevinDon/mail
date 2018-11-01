2710df01ab3cd2b256ce5a2c31c9aef1f9cf531b
DevinDon <DevinDon@Foxmail.com>
2018-11-01 22:53:04 +0800

    Update: 更新根组件目录.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { MatModule } from './module/mat.module';
+import { FormModule } from './module/form.module';
+import { AppComponent } from './component/app/app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { SideComponent } from './component/side/side.component';
+import { HomeComponent } from './component/home/home.component';
+import { InboxComponent } from './component/inbox/inbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideComponent,
+    HomeComponent,
+    InboxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatModule,
+    FormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
