b3f388e6d096b6394beb0f4318fdc862e3bec81d
DevinDon <DevinDon@Foxmail.com>
2018-11-03 00:46:19 +0800

    Update: 为侧边栏 ngFor 添加 trackBy 以优化性能.

--- src/app/component/side/side.component.ts
+++ src/app/component/side/side.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/service/user.service';
+import { MatDialog } from '@angular/material';
+import { ComposeDialogComponent } from '../dialog/compose-dialog/compose-dialog.component';
 
 @Component({
   selector: 'app-side',
@@ -9,13 +11,25 @@ import { UserService } from 'src/app/service/user.service';
 export class SideComponent implements OnInit {
 
   constructor(
-    public user: UserService
+    public user: UserService,
+    public dialog: MatDialog
   ) { }
 
   ngOnInit() { }
 
+  compose() {
+    this.dialog.open(ComposeDialogComponent, {
+      disableClose: false,
+      panelClass: 'no-padding'
+    });
+  }
+
   now() {
     return Date.now();
+  }
+
+  trackByFn(index: number, item): string {
+    return item.name;
   }
 
 }
