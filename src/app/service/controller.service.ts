714f17d54f58a1262206d1b808b2751ec6806f77
DevinDon <DevinDon@Foxmail.com>
2018-11-03 21:12:58 +0800

    Addition: 添加侧边导航栏控制项.

--- src/app/service/controller.service.ts
+++ src/app/service/controller.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ComposeDialogComponent } from '../component/dialog/compose-dialog/compose-dialog.component';
+import { Mail } from '../type';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControllerService {
 
-  constructor() { }
+  public toggle: Subject<string> = new Subject<string>();
+
+  constructor(
+    private dialog: MatDialog
+  ) { }
+
+  /**
+   * 新邮件窗口.
+   */
+  openDialogCompose(mail?: Mail): void {
+    this.dialog.open(ComposeDialogComponent, {
+      autoFocus: false,
+      disableClose: true,
+      panelClass: 'no-padding',
+      data: mail
+    });
+  }
+
+  sideToggle(tag: string = 'toggle') {
+    this.toggle.next(tag);
+  }
+
 }
