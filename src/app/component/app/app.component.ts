10cf10d036f921e919fae94b4c77f7d03cd399c0
DevinDon <DevinDon@Foxmail.com>
2018-11-03 21:14:12 +0800

    Addition: 新增对侧边导航栏的控制代码.

--- src/app/component/app/app.component.ts
+++ src/app/component/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatSidenav } from '@angular/material';
+import { ControllerService } from 'src/app/service/controller.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mail';
+export class AppComponent implements OnInit {
+
+  @ViewChild('sidenav')
+  public sidenav: MatSidenav;
+
+  title = 'Mail';
+
+  constructor(
+    private controller: ControllerService
+  ) { }
+
+  ngOnInit() {
+    this.controller.toggle.subscribe(v => {
+      switch (v) {
+        case 'toggle': this.sidenav.toggle(); break;
+        case 'open': this.sidenav.open(); break;
+        case 'close': this.sidenav.close(); break;
+        default: this.sidenav.toggle(); break;
+      }
+    });
+  }
+
 }
