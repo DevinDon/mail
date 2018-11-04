cbe0dff8e6d78195a6266f4cb97d3895cfc62c36
DevinDon <DevinDon@Foxmail.com>
2018-11-04 15:17:46 +0800

    Addition: 根据设备类型选择新邮件窗口配置; 侧边导航栏打开 / 关闭功能.

--- src/app/service/controller.service.ts
+++ src/app/service/controller.service.ts
@@ -1,34 +1,68 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { ComposeDialogComponent } from '../component/dialog/compose-dialog/compose-dialog.component';
-import { Mail } from '../type';
-import { Subject } from 'rxjs';
+import { Mail, DeviceType } from '../type';
+import { Subject, fromEvent } from 'rxjs';
+import { composeDialogConfig } from '../config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControllerService {
 
-  public toggle: Subject<string> = new Subject<string>();
+  /** 设备类型及屏幕尺寸. */
+  public deviceType: DeviceType;
+  /** 组件通讯服务: 侧边导航栏展开 / 关闭. */
+  public observableSidenavToggle: Subject<string>;
 
   constructor(
     private dialog: MatDialog
-  ) { }
+  ) {
+    this.observableSidenavToggle = new Subject<string>();
+    this.deviceType = this.getDeviceType();
+    fromEvent(window, 'resize').subscribe(event => this.deviceType = this.getDeviceType());
+  }
+
+  /**
+   * 获取设备类型及屏幕尺寸.
+   * @returns 设备类型及屏幕尺寸.
+   */
+  getDeviceType(): DeviceType {
+    if (innerWidth <= 320) { // 小屏手机
+      return { type: 'mobile', size: 's' };
+    } else if (innerWidth <= 375) { // 中屏手机
+      return { type: 'mobile', size: 'm' };
+    } else if (innerWidth <= 425) { // 大屏手机
+      return { type: 'mobile', size: 'l' };
+    } else if (innerWidth <= 768) { // 平板
+      return { type: 'table', size: 'm' };
+    } else if (innerWidth <= 1024) { // 大平板 或 小尺寸桌面
+      return { type: 'desktop', size: 's' };
+    } else if (innerWidth <= 1440) { // 中等尺寸桌面
+      return { type: 'desktop', size: 'm' };
+    } else if (innerWidth <= 1920) { // 大尺寸桌面
+      return { type: 'desktop', size: 'l' };
+    } else if (innerWidth <= 2560) { // 2K 桌面
+      return { type: 'desktop', size: 'l' };
+    } else { // 巨型展示荧幕
+      return { type: 'desktop', size: 'l' };
+    }
+  }
 
   /**
    * 新邮件窗口.
+   * @param mail 邮件内容.
    */
   openDialogCompose(mail?: Mail): void {
-    this.dialog.open(ComposeDialogComponent, {
-      autoFocus: false,
-      disableClose: true,
-      panelClass: 'no-padding',
-      data: mail
-    });
+    this.dialog.open(ComposeDialogComponent, composeDialogConfig[this.deviceType.type].default);
   }
 
-  sideToggle(tag: string = 'toggle') {
-    this.toggle.next(tag);
+  /**
+   * 开启或关闭侧边导航栏.
+   * @param tag 对侧边导航栏进行的操作.
+   */
+  sideToggle(tag: string = 'toggle'): void {
+    this.observableSidenavToggle.next(tag);
   }
 
 }
