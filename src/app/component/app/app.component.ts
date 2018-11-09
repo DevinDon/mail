bc1fb58c59f29dc94341f35a1b3732576824f300
DevinDon <DevinDon@Foxmail.com>
2018-11-09 09:18:46 +0800

    Update: Add new animations. Add footer / header animation. Fix sidenav display mode in mobile.

--- src/app/component/app/app.component.ts
+++ src/app/component/app/app.component.ts
@@ -1,8 +1,8 @@
-import { transition, trigger, useAnimation, query, style, animateChild, animate, group } from '@angular/animations';
+import { transition, trigger, useAnimation, query, group, style, animate, animateChild } from '@angular/animations';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material';
 import { ActivatedRoute, Router, RoutesRecognized } from '@angular/router';
-import { fromOpaqueFromRightToLeftOut, fromTransFromLeftToRightIn, fromTransFromRightToLeftIn, TimeLine, fromOpaqueFromLeftToRightOut } from 'src/app/others/animation';
+import { fromOpaqueFromBottomToTopOut, fromOpaqueFromRightToLeftOut, fromOpaqueFromTopToBottomOut, fromTransFromBottomToTopIn, fromTransFromLeftToRightIn, fromTransFromRightToLeftIn, fromTransFromTopToBottomIn, TimeLine, fromOpaqueOut, fromTransIn } from 'src/app/others/animation';
 import { ControllerService } from 'src/app/service/controller.service';
 
 @Component({
@@ -10,38 +10,18 @@ import { ControllerService } from 'src/app/service/controller.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    trigger('router', [
-      transition('* <=> *', [
-        useAnimation(fromTransFromRightToLeftIn, { params: { time: TimeLine.in } })
-        // query(':enter', [
-        //   style({ opacity: 0 }),
-        // ], { optional: true }),
-        // query(':leave', [
-        //   style({ opacity: 1 }),
-        //   animate('1s', style({ opacity: 0 }))
-        // ], { optional: true }),
-        // query(':enter', [
-        //   style({ opacity: 0 }),
-        //   animate('1s', style({ opacity: 1 }))
-        // ], { optional: true })
-      ])
+    trigger('header', [
+      transition(':enter', [useAnimation(fromTransFromTopToBottomIn, { params: { time: TimeLine.delayIn } })]),
+      transition(':leave', [useAnimation(fromOpaqueFromBottomToTopOut, { params: { time: TimeLine.delayOut } })])
     ]),
     trigger('content', [
-      transition('void => left', [
-        useAnimation(fromTransFromLeftToRightIn, {
-          params: { time: TimeLine.delayIn }
-        })
-      ]),
-      transition('void => right', [
-        useAnimation(fromTransFromRightToLeftIn, {
-          params: { time: TimeLine.delayIn }
-        })
-      ]),
-      transition(':leave', [
-        useAnimation(fromOpaqueFromRightToLeftOut, {
-          params: { time: TimeLine.out }
-        })
-      ])
+      transition('void => left', [useAnimation(fromTransFromLeftToRightIn, { params: { time: TimeLine.delayIn } })]),
+      transition('void => right', [useAnimation(fromTransFromRightToLeftIn, { params: { time: TimeLine.delayIn } })]),
+      transition('* => *', [useAnimation(fromTransIn, { params: { time: TimeLine.in } })])
+    ]),
+    trigger('footer', [
+      transition(':enter', [useAnimation(fromTransFromBottomToTopIn, { params: { time: TimeLine.delayIn } })]),
+      transition(':leave', [useAnimation(fromOpaqueFromTopToBottomOut, { params: { time: TimeLine.delayOut } })])
     ])
   ]
 })
@@ -50,14 +30,21 @@ export class AppComponent implements OnInit {
   /** 侧边栏. */
   @ViewChild('sidenav')
   private sidenav: MatSidenav;
-  public page: string;
+  /** 顶栏动画状态. */
+  public headerAnimationState: string;
+  /** 内容区动画状态. */
+  public contentAnimationState: string;
+  /** 页脚动画状态. */
+  public footerAnimationState: string;
 
   constructor(
-    public router: Router,
     public route: ActivatedRoute,
+    public router: Router,
     public controller: ControllerService
   ) {
-    this.page = 'void';
+    this.headerAnimationState = 'here';
+    this.contentAnimationState = 'right';
+    this.footerAnimationState = 'here';
   }
 
   ngOnInit() {
@@ -69,11 +56,13 @@ export class AppComponent implements OnInit {
         default: this.sidenav.toggle(); break;
       }
     });
-    this.router.events.subscribe(event => {
-      if (event instanceof RoutesRecognized) {
-        this.page = event.state.root.firstChild.data['animation'];
-      }
-    });
+    setTimeout(() => {
+      this.router.events.subscribe(event => {
+        if (event instanceof RoutesRecognized) {
+          this.contentAnimationState = event.state.root.firstChild.data['animation'];
+        }
+      });
+    }, 1500);
   }
 
 }
