2d9db848faa056ef36ef234dc48c9cdcb90ca050
DevinDon <DevinDon@Foxmail.com>
2018-11-09 09:16:52 +0800

    Remove: Remove home and page-not-found components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,11 @@ import { AppComponent } from './component/app/app.component';
 import { InboxComponent } from './component/folder/inbox/inbox.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HeaderComponent } from './component/header/header.component';
-import { HomeComponent } from './component/home/home.component';
-import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 import { SideComponent } from './component/side/side.component';
 import { DialogModule } from './module/dialog.module';
 import { MatModule } from './module/mat.module';
 import { OtherModule } from './module/other.module';
 import { RoutingModule } from './module/routing.module';
-
 
 @NgModule({
   declarations: [
@@ -19,9 +16,7 @@ import { RoutingModule } from './module/routing.module';
     HeaderComponent,
     FooterComponent,
     SideComponent,
-    HomeComponent,
-    InboxComponent,
-    PageNotFoundComponent
+    InboxComponent
   ],
   imports: [
     BrowserModule,
