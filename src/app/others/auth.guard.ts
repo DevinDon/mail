508c90a098563cc01d14c80419ab98c968675ef6
DevinDon <DevinDon@Foxmail.com>
2018-11-10 01:27:12 +0800

    Addition: Route Auth Guard.

---
+++ src/app/others/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../service/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private user: UserService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.user.isSignIn()) {
+      return true;
+    } else {
+      this.router.navigate(['sign', 'in']);
+      return false;
+    }
+  }
+
+}
