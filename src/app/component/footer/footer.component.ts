977fee248de0e7dcf026af55ccb6d84b59afa931
DevinDon <DevinDon@Foxmail.com>
2018-11-09 09:14:14 +0800

    Update: Move header / footer animation to the app component.

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,25 +1,9 @@
-import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { TimeLine, fromTransFromBottomToTopIn, fromOpaqueFromTopToBottomOut } from 'src/app/others/animation';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css'],
-  animations: [
-    trigger('footer', [
-      transition(':enter', [
-        useAnimation(fromTransFromBottomToTopIn, {
-          params: { time: TimeLine.delayIn }
-        })
-      ]),
-      transition(':leave', [
-        useAnimation(fromOpaqueFromTopToBottomOut, {
-          params: { time: TimeLine.delayOut }
-        })
-      ])
-    ])
-  ]
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
 
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,27 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'src/app/service/user.service';
 import { ControllerService } from 'src/app/service/controller.service';
-import { trigger, transition, useAnimation } from '@angular/animations';
-import { fromTransFromTopToBottomIn, TimeLine, fromOpaqueFromBottomToTopOut } from 'src/app/others/animation';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
-  animations: [
-    trigger('header', [
-      transition(':enter', [
-        useAnimation(fromTransFromTopToBottomIn, {
-          params: { time: TimeLine.delayIn }
-        })
-      ]),
-      transition(':leave', [
-        useAnimation(fromOpaqueFromBottomToTopOut, {
-          params: { time: TimeLine.delayOut }
-        })
-      ])
-    ])
-  ]
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
