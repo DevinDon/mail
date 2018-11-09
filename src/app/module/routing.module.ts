e68b048b68560467fbdd9720c6578e53caed06f3
DevinDon <DevinDon@Foxmail.com>
2018-11-09 09:15:42 +0800

    Update: Add costom data animation to the routes.

--- src/app/module/routing.module.ts
+++ src/app/module/routing.module.ts
@@ -1,32 +1,33 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from '../component/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ExceptionPageComponent } from 'exception-page';
 import { InboxComponent } from '../component/folder/inbox/inbox.component';
-import { PageNotFoundComponent } from '../component/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: HomeComponent,
-    data: { animation: 'Home' }
+    component: ExceptionPageComponent,
+    data: { animation: 'home' }
   }, {
     path: 'folder',
     pathMatch: 'full',
-    redirectTo: 'folder/inbox',
-    data: { animation: 'Folder' }
+    redirectTo: 'folder/inbox'
   }, {
     path: 'folder/inbox',
     component: InboxComponent,
-    data: { animation: 'Inbox' }
+    data: { animation: 'inbox' }
   }, {
     path: 'todoevent/:id',
-    component: HomeComponent,
-    data: { hello: 'hello' }
+    component: ExceptionPageComponent,
+    data: { animation: 'todoevent' }
   }, {
     path: '**',
-    component: PageNotFoundComponent,
-    data: { animation: 'All' }
+    component: ExceptionPageComponent,
+    data: {
+      animation: 'exception',
+      exception: 404
+    }
   }
 ];
 
