35b17dbbed00f07e1d511d68f63d1ab6bbc9261a
DevinDon <DevinDon@Foxmail.com>
2018-11-10 01:27:52 +0800

    Addition: Add test signin api.

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserInfo, Response } from '../others/type';
+import { SERVER } from '../others/config';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private client: HttpClient
+  ) { }
+
+  /**
+   * 向服务器发送登录请求.
+   * @param userInfo 用户登录信息.
+   */
+  signIn(userInfo: UserInfo): Observable<Response> {
+    return <Observable<Response>>this.client.post(SERVER.toURL('/signin'), userInfo);
+  }
 
 }
