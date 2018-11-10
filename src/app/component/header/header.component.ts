97b982c9e4691ac69d976c0157f0cce85053b6f8
DevinDon <DevinDon@Foxmail.com>
2018-11-10 14:39:51 +0800

    Optimization: Optimized routerLink.

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ControllerService } from 'src/app/service/controller.service';
 import { UserService } from 'src/app/service/user.service';
+import { ROUTERLIST } from 'src/app/others/config';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,8 @@ import { UserService } from 'src/app/service/user.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+  public routerList = ROUTERLIST;
 
   constructor(
     public user: UserService,
