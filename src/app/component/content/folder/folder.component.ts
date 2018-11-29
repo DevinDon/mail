82249e5c9a0f7011c9efa59d3ec4c9a115379cb0
DevinDon <DevinDon@Foxmail.com>
2018-11-30 00:26:45 +0800

    Addition: Use mat table to show data.

--- src/app/component/content/folder/folder.component.ts
+++ src/app/component/content/folder/folder.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { MatSort, MatPaginator, MatTableDataSource, MatPaginatorIntl } from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+import { fromEvent } from 'rxjs';
+import { distinctUntilChanged, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-folder',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FolderComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('inputFilter') filterInput: ElementRef<HTMLInputElement>;
+  @ViewChild('table') table: MatSort;
+  @ViewChild('paginator') paginator: MatPaginator;
+  public dataSource: MatTableDataSource<any>;
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.dataSource = new MatTableDataSource<any>();
+    this.dataSource.data = [];
+  }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.table;
+    this.dataSource.filterPredicate = (data, filter) => (data.title as string).indexOf(filter) !== -1;
+    fromEvent(this.filterInput.nativeElement, 'keyup')
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe(() => this.dataSource.filter = this.filterInput.nativeElement.value);
+    this.getData();
+  }
+
+  getData() {
+    this.http.get<any>('https://api.github.com/search/issues?q=repo:angular/material2&page=1')
+      .subscribe(data => this.dataSource.data = data.items);
   }
 
 }
