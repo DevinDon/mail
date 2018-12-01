4ef06c7bca79dc4056d679ac0346ceeaa2a27ffa
DevinDon <DevinDon@Foxmail.com>
2018-12-01 21:08:27 +0800

    Optimization: Optimized page transition animation.

--- src/app/component/app/app.component.ts
+++ src/app/component/app/app.component.ts
@@ -20,7 +20,7 @@ import { UserService } from 'src/app/service/user.service';
       transition('* <=> *', [
         animate('2s', keyframes([
           style({ opacity: 1, offset: 0 }),
-          style({ opacity: 0, offset: 0.5 }),
+          style({ opacity: 0.75, offset: 0.5 }),
           style({ opacity: 1, offset: 1 })
         ]))
       ])
