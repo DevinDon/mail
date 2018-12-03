ae4b6bfa790e1c607f1e5d8b24938871b639a924
DevinDon <DevinDon@Foxmail.com>
2018-12-03 22:33:33 +0800

    Update: Rename methods. Add postSignUp, postSignOut and getUser apis.

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,7 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { UserInfo, Response } from '../others/type';
-import { SERVER } from '../others/config';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Response, UserInfo } from '../others/type';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +16,29 @@ export class APIService {
    * 向服务器发送登录请求.
    * @param userInfo 用户登录信息.
    */
-  signIn(userInfo: UserInfo): Observable<Response<UserInfo>> {
+  postSignIn(userInfo: UserInfo): Observable<Response<UserInfo>> {
     return this.client.post<Response<UserInfo>>('http://localhost/v1/sign/in', userInfo);
+  }
+
+  /**
+   * 向服务器发送注册请求.
+   * @param userInfo 用户注册信息.
+   */
+  postSignUp(userInfo: UserInfo): Observable<Response<UserInfo>> {
+    return this.client.post<Response<UserInfo>>('http://localhost/v1/sign/up', userInfo);
+  }
+
+  /**
+   * 检测指定的项 (如用户名) 是否可被注册.
+   * @param params 用于检测的项目.
+   */
+  getUser(params: any) {
+    return this.client.get<Response<void>>('http://localhost/v1/user', { params });
+  }
+
+  /** 向服务器发送注销登录请求. */
+  postSignOut(): Observable<Response<any>> {
+    return this.client.post<Response<any>>('http://localhost/v1/sign/out', {});
   }
 
 }
