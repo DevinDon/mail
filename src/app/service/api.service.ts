8268bec208344ce0d6956886f24a740608766a48
DevinDon <DevinDon@Foxmail.com>
2018-12-04 23:40:34 +0800

    Update: Use api variable to locate server path.

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -8,16 +8,21 @@ import { Response, UserInfo } from '../others/type';
 })
 export class APIService {
 
+  private api: string;
+
   constructor(
     private client: HttpClient
-  ) { }
+  ) {
+    // this.api = 'api';
+    this.api = 'http://localhost';
+  }
 
   /**
    * 向服务器发送登录请求.
    * @param userInfo 用户登录信息.
    */
   postSignIn(userInfo: UserInfo): Observable<Response<UserInfo>> {
-    return this.client.post<Response<UserInfo>>('http://localhost/v1/sign/in', userInfo);
+    return this.client.post<Response<UserInfo>>(`${this.api}/v1/sign/in`, userInfo);
   }
 
   /**
@@ -25,7 +30,7 @@ export class APIService {
    * @param userInfo 用户注册信息.
    */
   postSignUp(userInfo: UserInfo): Observable<Response<UserInfo>> {
-    return this.client.post<Response<UserInfo>>('http://localhost/v1/sign/up', userInfo);
+    return this.client.post<Response<UserInfo>>(`${this.api}/v1/sign/up`, userInfo);
   }
 
   /**
@@ -33,12 +38,12 @@ export class APIService {
    * @param params 用于检测的项目.
    */
   getUser(params: any) {
-    return this.client.get<Response<void>>('http://localhost/v1/user', { params });
+    return this.client.get<Response<void>>(`${this.api}/v1/user`, { params });
   }
 
   /** 向服务器发送注销登录请求. */
-  postSignOut(): Observable<Response<any>> {
-    return this.client.post<Response<any>>('http://localhost/v1/sign/out', {});
+  postSignOut(info: any = {}): Observable<Response<any>> {
+    return this.client.post<Response<any>>(`${this.api}/v1/sign/out`, info);
   }
 
 }
